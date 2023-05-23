2991c48aaa98a677f7b4a4ba5e255973f9991e2f
TauroDevs <134343467+TauroDevs@users.noreply.github.com>
2023-05-22 21:13:38 -0500

    Second commit

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,10 @@
     const openButton = document.querySelector('.nav__menu');
     const menu = document.querySelector('.nav__link');
     const closeMenu = document.querySelector('.nav__close');
+    const navButton = document.querySelector('.nav__items');
+    const navButtonAcerca = document.querySelector('.nav__items__acerca')
+    const navButtonContacto = document.querySelector('.nav__items__contacto')
+    const navButtonPreguntas = document.querySelector('.nav__items__preguntas')
 
     openButton.addEventListener('click', ()=>{
         menu.classList.add('nav__link--show');
@@ -10,4 +14,21 @@
     closeMenu.addEventListener('click', ()=>{
         menu.classList.remove('nav__link--show');
     });
+
+    navButton.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    navButtonAcerca.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    navButtonContacto.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
+    navButtonPreguntas.addEventListener('click', ()=>{
+        menu.classList.remove('nav__link--show');
+    });
+
 })();
